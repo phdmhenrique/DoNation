3f15460030fa9961ba4a702e5f0972438b7ae840
phdmhenrique <pablo.monteirorgt19@gmail.com>
2024-11-23 15:59:13 -0300

    fix: removendo partes do código que não são utilizadas

--- src/Components/Divisory/Divisory.jsx
+++ src/Components/Divisory/Divisory.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Container } from './Divisory.js';
 
 function Divisory({ children }) {
--- src/Components/RightSide/RightSide.jsx
+++ src/Components/RightSide/RightSide.jsx
@@ -1,6 +1,3 @@
-import React from "react";
-
-// styled-components
 import { RightSideContainer, RightsideContent } from "./RightSide.js";
 
 function RightSide({ children }) {
